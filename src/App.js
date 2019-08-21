2818d0b372b54b7e872cbfedc8e59a2b8fdb1939
Christopher Harrison <cnharrison@gmail.com>
2019-08-20 20:21:16 -0400

    add PrivateRoute component, setup routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React from "react";
 import "./App.css";
 
+import { Route } from "react-router-dom";
+
 import Login from "./components/Login/Login.js";
 import Navbar from "./components/Navbar/Navbar.js";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute.js";
+import Dashboard from "./components/Dashboard/Dashboard.js";
+import Register from "./components/Register/Register.js";
 
 const App = props => {
   return (
     <div className="App">
-      <h1>🙊🙉🙈😎🗣👉anonymous feedback app👈🗣😎🙊🙉🙈</h1>
-      <Login />
-      <Navbar />
+      <h1>🙊🙉🙈😎🗣👉anonymous feedback app👈🗣😎🙊🙉🙈</h1>{" "}
+      <div>
+        <div>
+          <div>
+            <Navbar />
+          </div>
+          <div>
+            <Route exact path="/" component={Login} />
+          </div>
+          <div>
+            <Route path="/login" component={Login} />
+          </div>
+          <div>
+            <Route path="/signup" component={Register} />
+          </div>
+          <div>
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import Dashboard from "../Dashboard/Dashboard.js";
-import Register from "../Register/Register.js";
-
 const Login = props => {
   return (
     <div className="Login">
       <h1>Login component💻</h1>
-      <Dashboard />
-      <Register />
     </div>
   );
 };
