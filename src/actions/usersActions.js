73f68c291dd4b3825941256955d107c9538ef8a7
Christopher Harrison <cnharrison@gmail.com>
2019-08-24 14:09:05 -0400

    remove console logs, add payloads for register_sucess and reister_failure

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -5,17 +5,17 @@ export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAILURE = "REGISTER_FAILURE";
 
 export const register = newUser => dispatch => {
-   dispatch({type: REGISTER_START});
-   console.log(newUser);  
+  dispatch({ type: REGISTER_START });
 
-   return axios.post('https://anonymous-team-feedback.herokuapp.com/api/auth/register', newUser)
-   .then(res => {
-      console.log(res)
-      dispatch({type: REGISTER_SUCCESS});
-   })
-   .catch(err => {
-      console.log("register error:", err);
-      dispatch({ type: REGISTER_FAILURE });
-      return false
-      })
-}
+  return axios
+    .post(
+      "https://anonymous-team-feedback.herokuapp.com/api/auth/register",
+      newUser
+    )
+    .then(res => {
+      dispatch({ type: REGISTER_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: REGISTER_FAILURE, payload: err });
+    });
+};
