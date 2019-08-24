a22af6b3af1c1cac7015721bff9a5bedbb3ba2b8
Christopher Harrison <cnharrison@gmail.com>
2019-08-24 14:14:18 -0400

    add payloads to userReducer

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,36 +1,39 @@
-import { REGISTER_START, REGISTER_SUCCESS, REGISTER_FAILURE } from "../actions/usersActions.js";
+import {
+  REGISTER_START,
+  REGISTER_SUCCESS,
+  REGISTER_FAILURE
+} from "../actions/usersActions.js";
 
 const initialState = {
-    registering: false,
-    registerStartError: null,
-    registerFailureError: null
-
+  registering: false,
+  registerStartError: null,
+  registerFailureError: null
 };
 
 export const usersReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case REGISTER_START: {
-            return {
-                ...state,
-                registering: true,
-                registerStartError: null
-            }
-        }
-        case REGISTER_SUCCESS: {
-            return {
-                ...state,
-                registering: false
-            }
-        }
-        case REGISTER_FAILURE: {
-            return {
-                ...state,
-                registering: false,
-                registerFailureError: null
-            }
-        }
-
-        default:
-            return state;
+  switch (action.type) {
+    case REGISTER_START: {
+      return {
+        ...state,
+        registering: true,
+        registerStartError: action.payload
+      };
     }
+    case REGISTER_SUCCESS: {
+      return {
+        ...state,
+        registering: false
+      };
+    }
+    case REGISTER_FAILURE: {
+      return {
+        ...state,
+        registering: false,
+        registerFailureError: action.payload
+      };
+    }
+
+    default:
+      return state;
+  }
 };
