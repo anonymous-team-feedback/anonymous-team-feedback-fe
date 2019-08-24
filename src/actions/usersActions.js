72c30dfab63636943e47753eb043590806110bd5
Christopher Harrison <cnharrison@gmail.com>
2019-08-24 19:11:22 -0400

    save only token and _id in localstorage

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -1,38 +1,35 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const LOGIN_START = 'LOGIN_START';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const LOGIN_START = "LOGIN_START";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
 
 export function login(email, password, history) {
-    return dispatch => {
-        dispatch({ type: LOGIN_START });
+  return dispatch => {
+    dispatch({ type: LOGIN_START });
 
-        const endpoint = 'http://localhost:5050/api/auth/login';
-        const user = {
-            email,
-            password
-        };
+    const endpoint =
+      "http://anonymous-team-feedback.herokuapp.com/api/auth/login";
+    const user = {
+      email,
+      password
+    };
 
-        axios
-            .post(endpoint, user)
-            .then(res => {
-                localStorage.setItem('jwt', res.data.token);
-                localStorage.setItem('email', res.data.email);
-                localStorage.setItem('firstName', res.data.firstName);
-                localStorage.setItem('lastName', res.data.lastName);
-                localStorage.setItem('user_id', res.data.user_id);
-
-                dispatch({ type: LOGIN_SUCCESS, payload: res.data });
-                history.push('/');
-                console.log(user)
-                history.push('/');
-            })
-            .catch(err => {
-                dispatch({ type: LOGIN_FAILURE, payload: err })
-            })
-    }
-};
+    axios
+      .post(endpoint, user)
+      .then(res => {
+        localStorage.setItem("token", res.headers["x-auth-token"]);
+        localStorage.setItem("_id", res.data._id);
+        dispatch({ type: LOGIN_SUCCESS, payload: res.data });
+        history.push("/");
+        console.log(user);
+        history.push("/");
+      })
+      .catch(err => {
+        dispatch({ type: LOGIN_FAILURE, payload: err });
+      });
+  };
+}
 
 export const REGISTER_START = "REGISTER_START";
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
