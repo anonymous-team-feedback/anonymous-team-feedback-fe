14691506d79bb10aef5b2e10ee52ed86336aa88f
Christopher Harrison <cnharrison@gmail.com>
2019-08-24 15:28:29 -0400

    added temporary js alerts with server respoinse for debugging

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -15,9 +15,11 @@ export const register = newUser => dispatch => {
     .then(res => {
       localStorage.setItem("token", res.headers["x-auth-token"]);
       localStorage.setItem("_id", res.data._id);
+      alert(JSON.stringify(res.data));
       dispatch({ type: REGISTER_SUCCESS, payload: res.data });
     })
     .catch(err => {
+      alert(err);
       dispatch({ type: REGISTER_FAILURE, payload: err });
     });
 };
