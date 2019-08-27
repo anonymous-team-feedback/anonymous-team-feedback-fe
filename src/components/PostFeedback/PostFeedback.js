2ce9e10f25502f5fda0963269da341b5533bd341
Christopher Harrison <cnharrison@gmail.com>
2019-08-26 22:00:20 -0400

    conver to class component

--- src/components/PostFeedback/PostFeedback.js
+++ src/components/PostFeedback/PostFeedback.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Form, Modal, Icon, Header } from "semantic-ui-react"
+import { Form, Modal, Icon, Header } from "semantic-ui-react";
 import {
   DateInput,
   TimeInput,
   DateTimeInput,
   DatesRangeInput
-} from 'semantic-ui-calendar-react';
+} from "semantic-ui-calendar-react";
 
-
-const PostFeedback = props => {
-  return (
-    <div className="PostFeedback">
-      <h1>PostFeedback component📝</h1>
-    </div>
-  );
-};
+class PostFeedback extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div className="PostFeedback">
+        <h1>PostFeedback component📝</h1>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => {
   return {};
@@ -24,4 +26,4 @@ const mapStateToProps = state => {
 export default connect(
   mapStateToProps,
   {}
-)(PostFeedback)
+)(PostFeedback);
