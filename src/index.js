5cef5e879729bdaf1b3fa17636ca7c8d871f1432
Christopher Harrison <cnharrison@gmail.com>
2019-08-29 22:52:02 -0400

    add redux devtools extension

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import usersReducer from "./reducers/usersReducer";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import { BrowserRouter as Router } from "react-router-dom";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import "./index.css";
 
 import App from "./App";
 
-const store = createStore(combineReducers({ usersReducer }), applyMiddleware(thunk, logger));
+const store = createStore(combineReducers({ usersReducer }), composeWithDevTools(applyMiddleware(thunk, logger)));
 
 ReactDOM.render(
   <Router>
