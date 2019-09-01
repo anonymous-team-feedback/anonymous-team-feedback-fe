b98edbe447df09531908322b9cb6bed0a0d3abc4
Christopher Harrison <cnharrison@gmail.com>
2019-08-31 20:47:48 -0400

    check if there is feedback and send a message if there is not

--- src/components/ListFeedback/ListFeedback.js
+++ src/components/ListFeedback/ListFeedback.js
@@ -1,34 +1,35 @@
 import React from "react";
 import { connect } from "react-redux";
-import { withRouter } from 'react-router-dom';
-import { fetchAllPosts } from '../../actions/postsActions';
+import { withRouter } from "react-router-dom";
+import { fetchAllPosts } from "../../actions/postsActions";
 
 class ListFeedback extends React.Component {
-
-
   componentDidMount() {
     this.props.fetchAllPosts();
   }
 
-
   render() {
     return (
       <div className="Listfeedback">
-        <h1>ListFeedback component👂</h1>
-        {this.props.posts.map(post => (
-          <div key={post.id}>
-            <h1>{post.post}</h1>
-            <h1>{post.date}</h1>
+        {this.props.posts.length > 0 ? (
+          <div>
+            {this.props.posts.map(post => (
+              <div key={post.id}>
+                <h1>{post.post}</h1>
+                <h1>{post.date}</h1>
+              </div>
+            ))}
           </div>
-))}
+        ) : (
+          <p>You have no feedback.</p>
+        )}
       </div>
     );
-  };
   }
+}
 
-
-const MapStateToProps = ({ postsReducer: state}) => {
-  return {    
+const MapStateToProps = ({ postsReducer: state }) => {
+  return {
     posts: state.posts,
     loading: state.loading
   };
@@ -36,7 +37,7 @@ const MapStateToProps = ({ postsReducer: state}) => {
 
 export default withRouter(
   connect(
-  MapStateToProps,
-  { fetchAllPosts }
-)(ListFeedback)
+    MapStateToProps,
+    { fetchAllPosts }
+  )(ListFeedback)
 );
