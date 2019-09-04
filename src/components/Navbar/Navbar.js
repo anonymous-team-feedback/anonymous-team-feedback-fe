70b98cb2efe31432136e75417097ed71af094bbf
Tarmstrong95 <triston95strong@gmail.com>
2019-09-03 21:34:18 -0500

    Update styling for navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { connect } from "react-redux";
 import styled from 'styled-components';
+import { Link, withRouter } from 'react-router-dom';
 
 const Navbar = props => {
+  console.log(props)
   return (
     <NavBar className="Navbar">
       <div>
-        <Title>InCog</Title>
+        <Title to='/'>InCog</Title>
       </div>
-      {/* conditionally render login and signup routes */}
-      {!props.isLoggedIn && <form>
-        <Field name='Email' type='text' placeholder='email'/>
-        <Field name='Password' type='password' placeholder='password'/>
-        <Button type='submit'>Sign in</Button>
-      </form>}
+
+      {/* if not logged in, display inputs fields */}
+      {!props.isLoggedIn && props.location.pathname != '/login' && props.location.pathname != '/' &&
+        <form>
+          <Field name='Email' type='text' placeholder='email' />
+          <Field name='Password' type='password' placeholder='password' />
+          <Button type='submit'>Sign in</Button>
+          <ButtonLink to='/register'>Register</ButtonLink>
+        </form>
+      }
     </NavBar>
   );
 };
@@ -27,19 +33,25 @@ const mapStateToProps = state => {
 export default connect(
   mapStateToProps,
   {}
-)(Navbar);
+)(withRouter(Navbar));
 
 const NavBar = styled.nav`
 background-color: #393945;
 display: flex;
 justify-content: space-between;
-padding: 0.5rem 20rem 0.5rem 2rem;
+padding: 0.5rem 12rem 0.5rem 2rem;
 align-items: center;
 `
-const Title = styled.h1`
+const Title = styled(Link)`
 color: white;
 font-family: sans-serif;
 font-size: 1.8rem;
+font-weight: bold;
+transition: color .1s ease-in-out;
+
+:hover{
+  color: #51e3c2;
+}
 `
 const Field = styled.input`
 background-color: #4d505f;
@@ -58,4 +70,19 @@ font-weight: bold;
 padding: 0.3rem 0.6rem;
 font-size: 0.9rem;
 margin-left: 0.5rem;
+transition: .1s ease-in-out;
+:hover{
+  color: white;
+  background-color: #51e3c2;
+}
+`
+const ButtonLink = styled(Link)`
+color: #51e3c2;
+font-weight: bold;
+padding: 0.3rem 0.6rem;
+font-size: 0.9rem;
+margin-left: 0.5rem;
+:hover{
+  color: white;
+}
 `
