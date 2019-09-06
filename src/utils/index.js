bf8ad0da7317da0c1e673e1079163f932300d5d6
Tarmstrong95 <triston95strong@gmail.com>
2019-09-05 21:55:24 -0500

    Complete storage utility function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,19 @@
-export const clearStorage = () => {
-    
+// local storage utility funtion that receives a type of method, the key for the storage item and its value, in that order.
+export const storageUtil = (type, key, val) => {
+    switch (type) {
+        case 'setItem':
+            localStorage.setItem(key, val)
+            break;
+        case 'removeItem':
+            localStorage.removeItem(key)
+            break;
+        case 'getItem':
+            localStorage.getItem(key)
+            break;
+        case 'clear':
+            localStorage.clear()
+            break;
+        default:
+            break;
+    }
 }
