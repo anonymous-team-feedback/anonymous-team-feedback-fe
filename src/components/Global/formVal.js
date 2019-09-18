e4380a11b96e316f494cf80a2091d86ec8c1ba1d
Tarmstrong95 <triston95strong@gmail.com>
2019-09-18 00:31:45 -0500

    Create form validation popup global component

---
+++ src/components/Global/formVal.js
@@ -0,0 +1,38 @@
+// when using this component, set a component as a component prop called component
+import React from 'react'
+import { Button, Popup } from 'semantic-ui-react'
+
+
+class FormVal extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOpen: true
+        }
+    }
+
+    handleClose = () => {
+        this.setState({ isOpen: false })
+    }
+
+    render() {
+        return (
+            <Popup
+                trigger={this.props.comp}
+                content={`Invalid email or password. Please input the correct credentials!`}
+                on='click'
+                open={this.state.isOpen}
+                onClose={this.handleClose}
+                position='center'
+                style={{
+                    color: 'white',
+                    backgroundColor: '#c94940',
+                    border: 'none',
+                    maxWidth: '300px'
+                }}
+            />
+        )
+    }
+}
+
+export default FormVal
