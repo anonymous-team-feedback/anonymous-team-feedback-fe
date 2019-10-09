fadccc2470c9186752ee38c564731a4eb362d42d
Tarmstrong95 <triston95strong@gmail.com>
2019-10-09 19:00:54 +0700

    Conditionally render dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,24 +4,54 @@ import { withRouter } from "react-router-dom";
 
 import ListFeedback from "../ListFeedback/ListFeedback.js";
 import PostFeedback from "../PostFeedback/PostFeedback.js";
+import { Header } from "semantic-ui-react";
+
+import {fetchAllTeamMembers} from '../../actions/joinTeamRequestActions'
 
 
-const Dashboard = props => {
-  return (
-    <div className="Dashboard">
-      <ListFeedback />
-      <PostFeedback />
-    </div>
-  );
+class Dashboard extends React.Component {
+
+  componentDidMount(){
+    fetchAllTeamMembers()
+  }
+
+  render() {
+    return (
+<>
+        {this.props.slug && <div className="Dashboard">
+        <ListFeedback />
+        <PostFeedback />
+      </div>}
+
+      {!this.props.slug && <Pending/>}
+</>
+    );
+  }
 };
 
 const mapStateToProps = state => {
-  return {};
+  console.log('state', state)
+  return {
+    slug: state.slug
+  };
 };
 
 export default withRouter(
   connect(
     mapStateToProps,
-    {}
+    {fetchAllTeamMembers}
   )(Dashboard)
 );
+
+class Pending extends React.Component {
+  render(){
+    return (
+      <>
+      <Header as='h1'>
+        You are not part of a team quite yet. Please be patient while your manager approves your request. 
+      </Header>
+      <p>Thank you - SpeakIncog Team</p>
+      </>
+    )
+  }
+}
