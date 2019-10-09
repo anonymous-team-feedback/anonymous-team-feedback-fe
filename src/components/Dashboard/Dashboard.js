6e1ec494db57a2bad853408e7c6b74cc7c379ef2
Tarmstrong95 <triston95strong@gmail.com>
2019-10-09 22:27:49 +0700

    Remove clutter from dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -11,47 +11,26 @@ import {fetchAllTeamMembers} from '../../actions/joinTeamRequestActions'
 
 class Dashboard extends React.Component {
 
-  componentDidMount(){
-    fetchAllTeamMembers()
-  }
-
   render() {
     return (
-<>
-        {this.props.slug && <div className="Dashboard">
+      <div className="Dashboard">
         <ListFeedback />
         <PostFeedback />
-      </div>}
+      </div>
 
-      {!this.props.slug && <Pending/>}
-</>
     );
   }
 };
 
 const mapStateToProps = state => {
-  console.log('state', state)
   return {
-    slug: state.slug
+    slug: state.joinTeamRequestReducer.slug
   };
 };
 
 export default withRouter(
   connect(
     mapStateToProps,
-    {fetchAllTeamMembers}
+    { fetchAllTeamMembers }
   )(Dashboard)
 );
-
-class Pending extends React.Component {
-  render(){
-    return (
-      <>
-      <Header as='h1'>
-        You are not part of a team quite yet. Please be patient while your manager approves your request. 
-      </Header>
-      <p>Thank you - SpeakIncog Team</p>
-      </>
-    )
-  }
-}
