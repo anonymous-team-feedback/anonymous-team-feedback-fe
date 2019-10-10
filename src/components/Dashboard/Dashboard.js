95fa57c33e0cb3377551e4092dbdb5c91ba8f13a
Tarmstrong95 <triston95strong@gmail.com>
2019-10-10 23:32:49 +0700

    Conditionally render dashboard to display pending

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,27 +4,44 @@ import { withRouter } from "react-router-dom";
 
 import ListFeedback from "../ListFeedback/ListFeedback.js";
 import PostFeedback from "../PostFeedback/PostFeedback.js";
-import { Header } from "semantic-ui-react";
+import { Header, Message } from "semantic-ui-react";
 
-import {fetchAllTeamMembers} from '../../actions/joinTeamRequestActions'
+import { fetchAllTeamMembers } from '../../actions/joinTeamRequestActions'
+import { MainListContainer, SubListContainer, H2 } from "../ListFeedback/listFeedback-style.js";
 
 
 class Dashboard extends React.Component {
 
   render() {
-    return (
-      <div className="Dashboard">
-        <ListFeedback />
-        <PostFeedback />
-      </div>
-
-    );
+    if (!this.props.approved) {
+      return (
+        <MainListContainer>
+          <SubListContainer className="Listfeedback">
+            <H2>You don't appear to be in a team yet!</H2>
+            <Message negative>
+              <Message.Header>
+                Please wait for you team manager to approve you team request.
+              </Message.Header>
+              <p>Best Wishes - SpeakIncog Team</p>
+            </Message>
+          </SubListContainer>
+        </MainListContainer>
+      )
+    } else {
+      return (
+        <div className="Dashboard">
+          <ListFeedback />
+          <PostFeedback />
+        </div>
+      );
+    }
   }
 };
 
 const mapStateToProps = state => {
   return {
-    slug: state.joinTeamRequestReducer.slug
+    slug: state.joinTeamRequestReducer.slug,
+    approved: state.usersReducer.user.approved
   };
 };
 
