57494a613292573d90709bd755c043769dfe90d9
Tarmstrong95 <triston95strong@gmail.com>
2019-10-11 09:48:25 +0700

    Allow the user to select if they have requested

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,7 +4,7 @@ import { withRouter } from "react-router-dom";
 
 import ListFeedback from "../ListFeedback/ListFeedback.js";
 import PostFeedback from "../PostFeedback/PostFeedback.js";
-import { Header, Message } from "semantic-ui-react";
+import { Header, Message, Divider, Button } from "semantic-ui-react";
 
 import { fetchAllTeamMembers } from '../../actions/joinTeamRequestActions'
 import { MainListContainer, SubListContainer, H2 } from "../ListFeedback/listFeedback-style.js";
@@ -17,12 +17,24 @@ class Dashboard extends React.Component {
       return (
         <MainListContainer>
           <SubListContainer className="Listfeedback">
-            <H2>You don't appear to be in a team yet!</H2>
+            <Header as='h1' inverted>You don't appear to be in a team yet!</Header>
+            <Divider hidden/>
             <Message negative>
               <Message.Header>
-                Please wait for you team manager to approve you team request.
+                Please wait for you team manager to approve your team request.
               </Message.Header>
               <p>Best Wishes - SpeakIncog Team</p>
+            </Message>
+            <Divider />
+            <Message negative>
+              <Message.Header>
+                Tap the button below if you havent requested to join a team, yet.
+              </Message.Header>
+              <Divider hidden/>
+              <Button
+              content="Join/Create"
+              onClick={() => this.props.history.push('/jointeam')}
+              />
             </Message>
           </SubListContainer>
         </MainListContainer>
