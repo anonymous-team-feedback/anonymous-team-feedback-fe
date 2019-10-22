f38a3c7e5aa7b5ec2f0527630a98eae559113678
Tarmstrong95 <triston95strong@gmail.com>
2019-10-22 20:44:43 +0700

    Fix everything

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,21 +3,15 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
 import SideBar from "../SideBar/SideBar.js";
-import ListFeedback from "../ListFeedback/ListFeedback.js";
-import PostFeedback from "../PostFeedback/PostFeedback.js";
-import TeamMembersList from "../TeamMembersList/TeamMembersList.js";
-import { Header, Message, Divider, Button } from "semantic-ui-react";
 
-import { fetchAllTeamMembers } from '../../actions/joinTeamRequestActions'
-import { MainListContainer, SubListContainer, H2 } from "../ListFeedback/listFeedback-style.js";
 import NotApproved from "./NotApproved.js";
-import Pending from "./Pending.js";
+
 
 
 class Dashboard extends React.Component {
 
   render() {
-    if (this.props.members.includes(this.props.userId)) {
+    if (this.props.members && this.props.members.includes(this.props.userId)) {
       return (
         <div className="Dashboard"> 
         <SideBar />
@@ -47,6 +41,6 @@ const mapStateToProps = state => {
 export default withRouter(
   connect(
     mapStateToProps,
-    { fetchAllTeamMembers }
+    {  }
   )(Dashboard)
 );
